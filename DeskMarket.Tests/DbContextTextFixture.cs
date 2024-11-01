98772b6ee515fb72533a3ed947e1bf73392b6c1c
Lubomir <lubomir.pamukov@gmail.com>
2024-11-01 22:00:02 +0200

    Integrated testing library

---
+++ DeskMarket.Tests/DbContextTextFixture.cs
@@ -0,0 +1,24 @@
+﻿using DeskMarket.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeskMarket.Tests
+{
+    public class DbContextTextFixture : IDisposable
+    {
+        public ApplicationDbContext Context { get; private set; }
+
+        public DbContextTextFixture()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            Context = new ApplicationDbContext(options);
+        }
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
